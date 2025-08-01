ee7828a5448c691dfec037ea4ad49fe09f535c90
minhajul islam <nishatislam3108@gamil.com>
2025-08-01 18:03:04 +0600

    add support for client session

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import { SessionProvider } from "next-auth/react";
 import "./styles/globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
@@ -16,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        {children}
+        <SessionProvider>{children}</SessionProvider>
         <Toaster />
       </body>
     </html>
---
+++ src/hooks/useCurrentSession.js
@@ -0,0 +1,9 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
+export function useCurrentSession() {
+  const { data } = useSession();
+
+  return data?.user;
+}
